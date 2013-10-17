c106b53d92bd515a321a2f7a33324b1ee6c56b48
pentestscripts <jlshaw87@gmail.com>
2013-10-17 12:25:02 +0100

    Update Kali_Update.sh

--- Kali_Update.sh
+++ Kali_Update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-echo "[+] Updating Kali. Please wait."
-apt-get update > /dev/null
-apt-get upgrade -y > /dev/null
-apt-get dist-upgrade -y > /dev/null
-echo "[+] Kali Updated."
+clear
+
+echo -e "\e[1;36mKali Update Script\e[0m"
+
+echo "Press ENTER to continue, CTRL+C to abort."
+read input
+
+echo -e "\e[1;36m[+] Updating Kali. Please wait.\e[0m"
+apt-get update -y > /dev/null && apt-get upgrade -y > /dev/null && apt-get dist-upgrade -y > /dev/null
+echo -e "Kali is now up to date.\n"
+
+echo -e "\e[1;36m[+] Cleaning Kali. Please wait.\e[0m"
+apt-get autoclean -y > /dev/null && apt-get clean -y > /dev/null
+echo -e "\e[1;36m[+] Kali is now less dirty...Giggity\e[0m"
